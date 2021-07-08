3199c8472d1b93dde25c0e56dad94751355a9f83
Caitlin Moroney <57643150+caitmmoroney@users.noreply.github.com>
2021-07-08 14:20:56 -0400

    added scratch script to load & clean data

---
+++ nopd_app.R
@@ -0,0 +1,73 @@
+# R Shiny App for New Orleans Crime
+
+
+# 0. Load packages & data
+
+# install.packages("RSocrata")
+library(RSocrata)
+suppressPackageStartupMessages(library(dplyr))
+library(stringr)
+
+# Load csv files using URL
+policereport2021 <- read.csv("https://data.nola.gov/api/views/6pqh-bfxa/rows.csv?accessType=DOWNLOAD")
+
+'
+policereport2020 <- read.csv("https://data.nola.gov/api/views/hjbe-qzaz/rows.csv?accessType=DOWNLOAD")
+policereport2019 <- read.csv("https://data.nola.gov/api/views/mm32-zkg7/rows.csv?accessType=DOWNLOAD")
+policereport2018 <- read.csv("https://data.nola.gov/api/views/3m97-9vtw/rows.csv?accessType=DOWNLOAD")
+policereport2017 <- read.csv("https://data.nola.gov/api/views/qtcu-97s9/rows.csv?accessType=DOWNLOAD")
+policereport2016 <- read.csv("https://data.nola.gov/api/views/4gc2-25he/rows.csv?accessType=DOWNLOAD")
+policereport2015 <- read.csv("https://data.nola.gov/api/views/9ctg-u58a/rows.csv?accessType=DOWNLOAD")
+policereport2014 <- read.csv("https://data.nola.gov/api/views/6mst-xjhm/rows.csv?accessType=DOWNLOAD")
+policereport2013 <- read.csv("https://data.nola.gov/api/views/je4t-6qub/rows.csv?accessType=DOWNLOAD")
+policereport2012 <- read.csv("https://data.nola.gov/api/views/x7yt-gfg9/rows.csv?accessType=DOWNLOAD")
+policereport2011 <- read.csv("https://data.nola.gov/api/views/t596-ginn/rows.csv?accessType=DOWNLOAD")
+policereport2010 <- read.csv("https://data.nola.gov/api/views/s25y-s63t/rows.csv?accessType=DOWNLOAD")
+'
+
+# Load files using RSocrata
+pr2021_socrata <- read.socrata("https://data.nola.gov/resource/6pqh-bfxa.json")
+nopd_districts <- read.socrata("https://data.nola.gov/resource/6fjz-7kjs.json")
+
+'
+policereport2020 <- read.socrata("https://data.nola.gov/resource/hjbe-qzaz.json")
+policereport2019 <- read.socrata("https://data.nola.gov/resource/mm32-zkg7.json")
+policereport2018 <- read.socrata("https://data.nola.gov/resource/3m97-9vtw.json")
+policereport2017 <- read.socrata("https://data.nola.gov/resource/qtcu-97s9.json")
+policereport2016 <- read.socrata("https://data.nola.gov/resource/4gc2-25he.json")
+policereport2015 <- read.socrata("https://data.nola.gov/resource/9ctg-u58a.json")
+policereport2014 <- read.socrata("https://data.nola.gov/resource/6mst-xjhm.json")
+policereport2013 <- read.socrata("https://data.nola.gov/resource/je4t-6qub.json")
+policereport2012 <- read.socrata("https://data.nola.gov/resource/x7yt-gfg9.json")
+policereport2011 <- read.socrata("https://data.nola.gov/resource/t596-ginn.json")
+policereport2010 <- read.socrata("https://data.nola.gov/resource/s25y-s63t.json")
+'
+
+
+# 1. Data cleaning
+
+pr2021_socrata %>%
+  mutate(signal_type_category = str_remove(signal_type, "[A-Z]+")) ->
+  pr2021_socrata
+
+pr2021_socrata %>%
+  filter(persontype == "VICTIM") %>%
+  select(item_number, victim_number) %>%
+  group_by(item_number) %>%
+  mutate(n_victims = length(unique(victim_number))) %>%
+  select(item_number, n_victims) %>%
+  unique() ->
+  victim_nums
+
+pr2021_socrata %>%
+  filter(!is.na(offenderid)) %>%
+  select(item_number, offenderid) %>%
+  group_by(item_number) %>%
+  mutate(n_offenders = length(unique(offenderid))) %>%
+  select(item_number, n_offenders) %>%
+  unique() ->
+  offender_nums
+
+crime_info <- pr2021_socrata %>%
+  select(item_number, district, location, disposition, signal_type, signal_type_category, signal_description, occurred_date_time)
+
